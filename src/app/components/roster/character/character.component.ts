e91a1b0d0c3381141c171a30606848be8540cafd
ukon1990 <mail@jonaskf.net>
2017-06-25 13:04:58 +0200

    Raid view fixes

--- src/app/components/roster/character/character.component.ts
+++ src/app/components/roster/character/character.component.ts
@@ -12,6 +12,21 @@ declare const $WowheadPower: any;
 export class CharacterComponent implements OnInit, OnDestroy {
 	private sub: any;
 	character: any = {name: '', realm: ''};
+	pages: any = {raid: {count: 0, current: 1, perPage: 3}};
+	classBgColor: any  = {
+		1: '#1a0407', // Warrior
+		2: '#13040a', // Paladin
+		3: '#0f091b', // Hunter
+		4: '#160720"', // Rogue
+		5: '#15060e', // Priest
+		6: '#080812', // Death Knight
+		7: '#050414', // Shaman
+		8: '#110617', // Mage
+		9: '#080516', // Warlock
+		10: '#040b17', // Monk
+		11: '#04100a', // Druid
+		12: '#000900' // Demon hunter
+	};
 
 	constructor(private activatedRoute: ActivatedRoute,
 		private sanitizer: DomSanitizer, private characterService: CharacterService) { }
@@ -25,6 +40,8 @@ export class CharacterComponent implements OnInit, OnDestroy {
 					this.character = c.json();
 					console.log(this.character);
 					this.character.progression.raids.reverse();
+					this.pages.raid.count =
+						this.character.progression.raids.length / this.pages.raid.perPage;
 					$WowheadPower.init();
 				})
 				.catch(error => console.log(error));
@@ -35,6 +52,12 @@ export class CharacterComponent implements OnInit, OnDestroy {
 		this.sub.unsubscribe();
 	}
 
+	changePage(change: number, type: string): void {
+		if (this.pages[type].current + change > 0) {
+			this.pages[type].current += change;
+		}
+	}
+
 	getBackgroundImage() {
 		if (this.character.thumbnail) {
 			const url = `https://render-eu.worldofwarcraft.com/character/${
@@ -43,6 +66,10 @@ export class CharacterComponent implements OnInit, OnDestroy {
 			return this.sanitizer.bypassSecurityTrustStyle('url(' + url + ')');
 		}
 		return '';
+	}
+
+	getBackgroundColor() {
+		return this.sanitizer.bypassSecurityTrustStyle(this.classBgColor[this.character.class]);
 	}
 
 	bonusList(bonusList: any[]): string {
