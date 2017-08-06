812217c6bc59ae5a95f752941e3051c96ef124e9
ukon1990 <mail@jonaskf.net>
2017-08-06 20:20:45 +0200

    Achievements sorted

--- src/app/components/roster/character/character-achievement/character-achievement.component.ts
+++ src/app/components/roster/character/character-achievement/character-achievement.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-character-achievement',
@@ -8,9 +9,27 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CharacterAchievementComponent implements OnInit {
 	@Input() completedAchievements: any[];
 	@Input() achievementGroup: any;
-	constructor() { }
+	map = [];
+
+	constructor(private sanitizer: DomSanitizer) { }
 
 	ngOnInit() {
+		this.completedAchievements.forEach( a => {
+			this.map[a] = a;
+		});
+
+		this.achievementGroup.achievements.sort( (a: any, b: any) => this.map[a.id] && !this.map[b.id] ? -1 : 1 );
+		if (this.achievementGroup.categories) {
+			this.achievementGroup.categories.forEach(c => {
+				c.achievements.sort( (a: any, b: any) => this.map[a.id] && !this.map[b.id] ? -1 : 1 );
+			});
+		}
 	}
 
+
+	getIcon(achievement) {
+		return this.sanitizer.bypassSecurityTrustStyle(`url("https://render-eu.worldofwarcraft.com/icons/56/${
+			achievement.icon
+		}.jpg")`);
+	}
 }
