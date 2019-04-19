0884bb72b6194526ad822a99eb299eb597d712bf
Ukon1990 <mail@jonaskf.net>
2019-04-19 15:31:09 +0200

    .

--- server/handlers/auth.handler.ts
+++ server/handlers/auth.handler.ts
@@ -1,4 +1,5 @@
 import * as http from 'request';
+import * as btoa from 'btoa';
 import {BLIZZARD} from '../secrets';
 import {APIGatewayEvent, Callback} from 'aws-lambda';
 import {Response} from '../utils/response.util';
@@ -10,20 +11,35 @@ export class AuthHandler {
       code = body.code,
       redirectURI = body.redirectURI;
 
-    http.get(`https://${
+    http.post({
+      headers: {
+        'Authorization': `Basic ${btoa(`${BLIZZARD.CLIENT_ID}:${BLIZZARD.CLIENT_SECRET}`)}`
+      },
+      url: `https://${region}.battle.net/oauth/token`,
+      form: {
+        code: code,
+        redirect_uri: redirectURI,
+        grant_type: 'authorization_code',
+        scope: 'wow.profile sc2.profile'
+      }
+    },
+      (err, response, body) => {
+        Response.send(body, callback);
+
+      });/*
+    http.post(`https://${
         region
-        }.battle.net/oauth/token?grant_type=authorization_code&client_id=${
-        BLIZZARD.CLIENT_ID
-        }&client_secret=${
-        BLIZZARD.CLIENT_SECRET
-        }&code=${
-        code
-        }&redirect_uri=${
-        redirectURI
-        }`,
+        }.battle.net/oauth/token`, {
+        code: code,
+        redirect_uri: redirectURI,
+        grant_type: 'authorization_code',
+        scope: 'wow.profile sc2.profile',
+        client_id: BLIZZARD.CLIENT_ID,
+        client_secret: BLIZZARD.CLIENT_SECRET
+      },
       (err, response, body) => {
         Response.send(JSON.parse(body), callback);
 
-      });
+      });*/
   }
 }
