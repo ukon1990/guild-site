ed4f215ffc2abb6ede40304fa842b90a2089dca3
ukon <mail@jonaskf.net>
2023-01-10 14:48:11 +0100

    .

--- api/serverless.ts
+++ api/serverless.ts
@@ -5,7 +5,7 @@ import hello from '@functions/hello';
 const serverlessConfiguration: AWS = {
   service: 'api',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offlin  e'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
